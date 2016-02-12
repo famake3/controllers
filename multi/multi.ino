cc6f6c7e835663beabcc07a4d8f78506e5362e92
fa2k <pmb@fa2k.net>
2016-02-12 21:38:11 +0100

    Adding temp sensors

--- multi/multi.ino
+++ multi/multi.ino
@@ -1,22 +1,28 @@
 #include <IRremote.h>
 
-const int RELAYPIN = 2;
-const int TEMPPIN = A0;
+const int RELAY_PIN = 2;
+const int TEMP_PINS[] = {A0, A1};
+const int NUM_TEMP = 2;
+const int IR_PIN = 3;
+const int RF_RECV_PIN = 2;
+const int RF_SEND_PIN = 4; 
 
-const int FN_SCREEN = 1, FN_TEMP = 2, FN_IR = 3;
+const int FN_TEMP = 0x80, FN_SCREEN = 1, FN_IR = 3, FN_RF = 4;
 
-int prevTemp = -1;
+int prevTemp[NUM_TEMP];
 long prevReport = 0;
 
 IRsend irsend;
 
+
 void setup() {
   Serial.begin(115200);
-  pinMode(RELAYPIN, OUTPUT);
-  digitalWrite(RELAYPIN, HIGH);
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, HIGH);
 }
 
 void loop() {
+  int i;
   if (Serial) {
     if (Serial.available() >= 3) {
       if (Serial.read() != '!') return;
@@ -26,30 +32,37 @@ void loop() {
       Serial.readBytes(data, len);
       switch (funcId) {
         case FN_SCREEN:
-          digitalWrite(RELAYPIN, !data[0]);
-          break;
-        case FN_TEMP:
-          prevTemp = -1;
-          prevReport = 0;
+          digitalWrite(RELAY_PIN, !data[0]);
           break;
         case FN_IR:
           irSend();
           break;
       }
+      if (funcId >= FN_TEMP && funcId < FN_TEMP + NUM_TEMP) {
+        prevTemp[funcId - FN_TEMP] = 0;
+        prevReport = 0;
+      }
     }
-    updateTemperature();
+    long now = millis();
+    if (now > prevReport + 60000) {
+      for (i = 0; i<NUM_TEMP; ++i) {
+        updateTemperature(i);
+      }
+      prevReport = now;
+    }
   }
 }
 
-void updateTemperature() {
-    int temp = analogRead(TEMPPIN);
-    if (temp != prevTemp && millis() > prevReport + 60000) {
+void updateTemperature(int i_temp) {
+    int temp = analogRead(TEMP_PINS[i_temp]);
+    if (temp != prevTemp[i_temp]) {
       Serial.write('!');
-      Serial.write(FN_TEMP);
+      Serial.write(FN_TEMP + i_temp);
       Serial.write(2);
       Serial.write(temp >> 8);
       Serial.write(temp & 0xFF);
-      prevTemp = temp;
+      Serial.println(temp);
+      prevTemp[i_temp] = temp;
       prevReport = millis();
     }
 }
