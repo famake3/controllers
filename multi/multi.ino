59a017427ea374a4755b3759b7df778a4f3b6bff
fa2k <pmb@fa2k.net>
2016-03-06 21:41:12 +0100

    removing obsolete definitions

--- multi/multi.ino
+++ multi/multi.ino
@@ -24,8 +24,6 @@ const int SWITCH_PIN = 2;
 const int TEMP_PINS[] = {A0, A1};
 const int NUM_TEMP = 2;
 const int IR_PIN = 3;
-const int RF_RECV_PIN = 2;
-const int RF_SEND_PIN = 4;
 
 int prevTemp[NUM_TEMP];
 long prevReport = 0;
