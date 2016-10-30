66e11c345044aa634b904aef6d02fab642cd922c
Mikael Aasen <mikaelaasen@hotmail.com>
2016-10-30 15:32:52 +0000

    improved error handling

--- bevegelse.py
+++ bevegelse.py
@@ -4,7 +4,7 @@ import sys
 import paho.mqtt.client as mqtt
 from functools import partial
 
-PIN = 4
+PIN = 17
 
 def main(mqtt_server, topic):
 	client = mqtt.Client()
@@ -13,16 +13,21 @@ def main(mqtt_server, topic):
 	GPIO.setup(PIN, GPIO.IN)
 	prev = None
 	while True:
-		GPIO.wait_for_edge(PIN, GPIO.BOTH, timeout=60)
-		if GPIO.input(PIN):
-			payload = "ON"
-		else:
-			payload = "OFF"
+		try:
+			GPIO.wait_for_edge(PIN, GPIO.BOTH, timeout=60)
+			if GPIO.input(PIN):
+				payload = "ON"
+			else:
+				payload = "OFF"
 
-		if prev != payload:
-			client.publish(topic, payload)
+			if prev != payload:
+					client.publish(topic, payload)
 			prev = payload
-		client.loop()
+			client.loop()
+		except:
+			time.sleep(10)
+			client = mqtt.Client()
+			client.connect(mqtt_server, 1883, 60)
 
 if __name__ == "__main__":
 	main(sys.argv[1], sys.argv[2])
