8cb6a1d66c5efae56e83931a3d475011ff144c1f
The Pi <pi@example.com>
2016-11-27 11:47:19 +0000

    Try to protect against errors

--- bevegelse.py
+++ bevegelse.py
@@ -7,12 +7,18 @@ from functools import partial
 PIN = 17
 
 def main(mqtt_server, topic):
-	client = mqtt.Client()
-	client.connect(mqtt_server, 1883, 60)
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(PIN, GPIO.IN)
+	client = None
 	prev = None
 	while True:
+		while client == None:
+			try:
+				client = mqtt.Client()
+				client.connect(mqtt_server, 1883, 60)
+			except:
+				client = None
+				time.sleep(10)
 		try:
 			GPIO.wait_for_edge(PIN, GPIO.BOTH, timeout=60)
 			if GPIO.input(PIN):
@@ -26,8 +32,11 @@ def main(mqtt_server, topic):
 			client.loop()
 		except:
 			time.sleep(10)
-			client = mqtt.Client()
-			client.connect(mqtt_server, 1883, 60)
+			try:
+				client.disconnect()
+			except:
+				pass
+			client = None
 
 if __name__ == "__main__":
 	main(sys.argv[1], sys.argv[2])
