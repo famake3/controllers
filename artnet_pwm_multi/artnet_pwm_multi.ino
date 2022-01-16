849e6ba0ad8d1b900a101b97a15b123525c42b88
Marius Bjørnstad <pmb@fa2k.net>
2022-01-16 11:50:45 +0100

    add temperature function

--- artnet_pwm_multi/artnet_pwm_multi.ino
+++ artnet_pwm_multi/artnet_pwm_multi.ino
@@ -1,7 +1,6 @@
 #include <PubSubClient.h>
-#include <Ethernet2.h>
-#include <EthernetUdp.h>
-#include <EthernetClient.h>
+
+#include <Ethernet.h>
 
 byte mac[] = {
   0xFE, 0xED, 0xFA, 0xAA, 0xAA, 0xAA
@@ -10,7 +9,7 @@ IPAddress ip(192, 168, 1, 226);
 
 EthernetUDP udp;
 EthernetClient client;
-PubSubClient mqtt("192.168.1.2", 1883, client);
+PubSubClient mqtt("192.168.1.8", 1883, client);
 
 const int HEADER_LEN = 18, MAX_CHAN = 512;
 byte packet[HEADER_LEN + MAX_CHAN];
@@ -21,23 +20,27 @@ const int* ANT_UNIVERSE[] = {UNIVERSE_0, UNIVERSE_1};
 const int N_CHAN[] = {9, 3};
 const int N_UNIVERSE = sizeof(ANT_UNIVERSE) / sizeof(ANT_UNIVERSE[0]);
 
+const int TEMP_PINS[] = {A0};
+const int TEMP_FUDGE[] = {0};
+const int N_TEMP_SMOOTH = 16;
+const int TEMP_RATE_LIMIT_INTERVAL = 5000;
+const int N_TEMP_IN = sizeof(TEMP_PINS) / sizeof(TEMP_PINS[0]);
+const char* TEMP_TOPIC[] = {
+  "stue/temp"
+};
+int temp_last_out[N_TEMP_IN];
+long temp_last_time[N_TEMP_IN];
+int temp_adc_samples[N_TEMP_IN][N_TEMP_SMOOTH];
+long temp_adc_sum[N_TEMP_IN];
+int temp_adc_index[N_TEMP_IN];
+char temp_string_buffer[8];
+
 const int INPUT_PINNA = 14;
 int doorState = -1;
 
-const int STROBE_PINNA = 41;
-
-float strobe_freq = 10.0, strobe_power = 40.0;
-float strobe_duration = 500.0;
-
-const float DEF_STROBE_FREQ = 10.0;
-const float DEF_STROBE_DURN = 500.0;
-const float MAX_FREQ = 70.0;
-
-long strobe_last_cmd = 0;
-long strobe_last_blink_us = 0;
-bool strobe_active = false;
 
 void setup() {
+  analogReference(EXTERNAL);
   Ethernet.begin(mac, ip);
   udp.begin(0x1936);
   for (int uni=0; uni<N_UNIVERSE; ++uni) {
@@ -47,10 +50,6 @@ void setup() {
     }
   }
   pinMode(INPUT_PINNA, INPUT_PULLUP);
-  pinMode(STROBE_PINNA, OUTPUT);
-  digitalWrite(STROBE_PINNA, HIGH);
-  delayMicroseconds(2);
-  digitalWrite(STROBE_PINNA, LOW);
 }
 
 void loop() {
@@ -72,24 +71,33 @@ void loop() {
         // Art-Net packet
         if (universe < N_UNIVERSE) {
           const int* channels = ANT_UNIVERSE[universe];
-          for (int ch=0; ch<min(N_CHAN[universe], length); ++ch) {
+          int ch;
+          for (ch=0; ch<min(N_CHAN[universe], length); ++ch) {
             analogWrite(channels[ch], packet[HEADER_LEN + ch]);
-          }
-          if (universe == 0 && length > N_CHAN[universe]) {
-              // Extra channels in universe 0 are for stroboscope
-              int num_chan = length - N_CHAN[universe];
-              strobe_freq = (MAX_FREQ * packet[HEADER_LEN + ch]) / 256.0;
-              strobe_active = true;
           }
         }
       }
     }
   }
-  // 2. Strobe
-  long now = micros();
-  float period = 1e6 / strobe_freq;
-  if (now - strobe_last_blink_us >= period) {
+  long now = millis();
+  // 2. Analog in
+  for (int i=0; i<N_TEMP_IN; ++i) {
+    int val = analogRead(TEMP_PINS[i]) + TEMP_FUDGE[i];
+    // volt = (5 * val / 1023)
+    // cels = (volt - 0.5) * 100
+    temp_adc_samples[i][temp_adc_index[i]] = val;
+    temp_adc_sum[i] += val;
+    temp_adc_index[i] = (temp_adc_index[i] + 1) % N_TEMP_SMOOTH;
+    temp_adc_sum[i] -= temp_adc_samples[i][temp_adc_index[i]];
+    int deciCelsius = (int)(((3300L * temp_adc_sum[i]) / 1023) / N_TEMP_SMOOTH - 500);
+    if ((now - temp_last_time[i]) > TEMP_RATE_LIMIT_INTERVAL && deciCelsius != temp_last_out[i]) {
+      dtostrf(deciCelsius * 0.1, 4, 1, temp_string_buffer);
+      mqtt.publish(TEMP_TOPIC[i], temp_string_buffer);
+      temp_last_out[i] = deciCelsius;
+      temp_last_time[i] = now;
+    }
   }
+
   // 3. Input: door fooler
   int door = digitalRead(INPUT_PINNA) == HIGH ? 1 : 0;
   if (door != doorState) {
@@ -106,5 +114,3 @@ int getTwo(byte* data, int index) {
 int getTwoTwo(byte* data, int index) {
   return (data[index+1] << 8) + (data[index]);
 }
-
-
