3ae1e4d9331da8b5513f944cd1069644814bdc3c
Marius Bjørnstad <pmb@fa2k.net>
2023-05-03 20:43:58 +0200

    window controller

---
+++ mqtt-pi-node/vindu.py
@@ -0,0 +1,125 @@
+import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO
+import time
+from threading import Event
+
+OPEN_PIN = 2
+CLOSE_PIN = 3
+
+mqtt_server = "192.168.1.8"
+mqtt_port = 1883
+
+fullOpenTime = 44000
+targetPercentage = 0.0
+currentPercentage = 0.0
+currentMode = 0
+alignFirst = 0
+ALIGN_AMOUNT = 2.0
+alignAmountRemain = 0.0
+newCommandEvt = Event()
+
+def mqttCallback(client, userdata, message):
+    global targetPercentage, alignFirst, alignAmountRemain
+    payload = message.payload.decode()
+    topic = message.topic
+
+    if topic == "soverom/vindu/aapning":
+        if payload == "ON":
+            targetPercentage = 100.0
+        elif payload == "OFF":
+            targetPercentage = 0.0
+        elif payload == "DECREASE":
+            targetPercentage = max(0, targetPercentage - 1)
+        elif payload == "INCREASE":
+            targetPercentage = min(100, targetPercentage + 1)
+        elif payload.isalnum():
+            targetPercentage = min(max(int(payload), 0), 100)
+        else:
+            return
+
+        if targetPercentage < 20:
+            alignFirst = -1
+            alignAmountRemain = ALIGN_AMOUNT
+        elif targetPercentage > 80:
+            alignFirst = 1
+            alignAmountRemain = ALIGN_AMOUNT
+        else:
+            alignFirst = 0
+
+        newCommandEvt.set()
+
+    elif topic == "soverom/vindu/konf/tid":
+        fullOpenTime = int(payload)
+
+
+def controlWindow(client):
+    global currentPercentage, currentMode, alignFirst, alignAmountRemain
+
+    lastCommandTime = 0
+    while True:
+        currentTime = time.time() * 1000
+        newPercentage = currentPercentage
+
+        if currentMode != 0:
+            elapsedTime = currentTime - lastCommandTime
+            deltaPercentage = 100.0 * elapsedTime * currentMode / fullOpenTime
+            newPercentage = max(0, min(100, currentPercentage + deltaPercentage))
+
+            if alignFirst == -1 and newPercentage == 0.0:
+                alignAmountRemain += deltaPercentage
+            elif alignFirst == 1 and newPercentage == 100.0:
+                alignAmountRemain -= deltaPercentage
+            if alignAmountRemain < 0:
+                alignFirst = 0
+
+        if alignFirst != 0:
+            currentMode = alignFirst
+            lastCommandTime = currentTime
+        elif (targetPercentage - currentPercentage) > 0.5:
+            currentMode = 1
+            lastCommandTime = currentTime
+        elif (currentPercentage - targetPercentage) > 0.5:
+            currentMode = -1
+            lastCommandTime = currentTime
+        else:
+            currentMode = 0
+
+        GPIO.output(OPEN_PIN, GPIO.HIGH if currentMode == 1 else GPIO.LOW)
+        GPIO.output(CLOSE_PIN, GPIO.HIGH if currentMode == -1 else GPIO.LOW)
+
+        if newPercentage != currentPercentage:
+            currentPercentage = newPercentage
+            newPct = str(currentPercentage)
+            client.publish("soverom/vindu/aapningStatus", newPct)
+        
+        if currentMode == 0:
+            newCommandEvt.wait(timeout=10.0)
+        else:
+            newCommandEvt.wait(timeout=0.5)
+
+
+def main():
+    client = mqtt.Client()
+    client.on_message = mqttCallback
+    client.connect(mqtt_server, mqtt_port)
+
+    client.loop_start()
+    client.subscribe("soverom/vindu/aapning")
+    client.subscribe("soverom/vindu/konf/tid")
+    client.publish("soverom/vindu/oppstart", "ON")
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(OPEN_PIN, GPIO.OUT)
+    GPIO.setup(CLOSE_PIN, GPIO.OUT)
+
+    GPIO.output(OPEN_PIN, GPIO.LOW)
+    GPIO.output(CLOSE_PIN, GPIO.LOW)
+
+    try:
+        controlWindow(client)
+    finally:
+        GPIO.cleanup()
+        client.loop_stop()
+
+if __name__ == "__main__":
+    main()
